7b53b2d50ea4801fb6fbaf7e3ecfc13c178efd47
unknown <birhanuwoeldmariam@gmail.com>
2024-03-13 20:52:28 +0300

    final3

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -8,13 +8,9 @@ const dbConnection = mysql2.createPool({
     password:"A123456789a", 
     connectionLimit: "10"
 
-  //  user:process.env.USER, 
-  //   database:process.env.DATABASE,
-  //   host:"localhost",
-  //   password:process.env.PASSWORD, 
-  //   connectionLimit: "10"
+  
 });
-  console.log(process.env.USER)
+  // console.log(process.env.USER)
   // console.log(process.env.PASSWORD)
   // console.log(process.env.DATABASE)
 module.exports = dbConnection.promise();
